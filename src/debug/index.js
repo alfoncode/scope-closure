6c21b37f414d1628841c5046b947497ba00e79cd
alfoncode <ajgarciaba@gmail.com>
2021-12-08 19:05:41 +0100

    Change debug example

--- src/debug/index.js
+++ src/debug/index.js
@@ -11,3 +11,19 @@ function hello() {
 }
 
 hello();
+
+const moneyBox = () => {
+    debugger;
+    var saveCoins = 0;
+    const countCoins = (coins) => {
+        saveCoins += coins;
+        console.log(`MoneyBox: $${saveCoins}`);
+    }
+    return countCoins;
+};
+
+let myMoneyBox = moneyBox();
+
+myMoneyBox(4);
+myMoneyBox(6)
+myMoneyBox(10);
